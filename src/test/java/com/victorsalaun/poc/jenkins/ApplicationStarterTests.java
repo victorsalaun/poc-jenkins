11dff1eed446686d4301c4d2d0061454b5b646b1
Victor <victor.salaun@softeam.com>
2017-03-30 16:03:48 +0200

    add java project

---
+++ src/test/java/com/victorsalaun/poc/jenkins/ApplicationStarterTests.java
@@ -0,0 +1,16 @@
+package com.victorsalaun.poc.jenkins;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationStarterTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
